3d7b0fd9fd7ebfa408a2e7d7b049253fc23b656c
vovasavchuk <vovasavchuk2014@gmail.com>
2023-06-26 09:48:44 +0300

    test

---
+++ server/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use('/website',express.static(path.join(__dirname, 'public')));
+
+app.use('/website',express.static(path.join(__dirname, 'public')));
+
+app.use((req, res) => {
+    res.status(404);
+    res.send(`<h1>Error 404: Resource not found</h1>`)
+})
+
+app.listen(3000, () => {
+    console.log('App listening on port 3000');
+})
